8592d1bde22b34f547aa088ead7f910186bdefe7
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-19 19:11:50 -0700

    chnaged the switch statement to if else

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,13 +17,6 @@ $(document).ready(() => {
     compose: $('.compose__box')[0],
     newTweet: $('.container .new-tweet')[0]
   };
-  // tweets_container: document.getElementById('tweets__container'),
-  // errorParagraph: document.querySelector('.error__message'),
-  // tweetForm: document.querySelector('.new-tweet form'),
-  // textarea: document.querySelector('.new-tweet form > textarea'),
-  // counter: document.querySelector('.form__footer .counter'),
-  // compose: document.querySelector('.compose__box'),
-  // newTweet: document.querySelector('.container .new-tweet')
 
   console.log('DOM fully loaded and parsed from app.js');
   // data variable
@@ -38,18 +31,6 @@ $(document).ready(() => {
   };
 
   // get tweets with ajax
-  // const loadTweets = () => {
-  //   const request = new XMLHttpRequest();
-  //   request.open('GET', '/tweets', true);
-  //   request.onreadystatechange = function() {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //       data = JSON.parse(this.responseText);
-  //       renderTweets(data);
-  //     }
-  //   };
-  //   request.send();
-  // };
-  // loadTweets();
 
   const loadTweets = () => {
     $.ajax({
@@ -81,33 +62,6 @@ $(document).ready(() => {
     return error;
   };
 
-  // callback function for submitting a tweet
-  // const submitTweet = e => {
-  //   event.preventDefault();
-  //   let errorMessage = '';
-  //   const tweetText = elementsMap.textarea.value;
-  //   if (!hasError(tweetText)) {
-  //     errorMessage = '';
-  //     if (event.keyCode === 13 || e.type === 'submit') {
-  //       const request = new XMLHttpRequest();
-  //       request.open('POST', '/tweets', true);
-  //       request.setRequestHeader(
-  //         'Content-Type',
-  //         'application/x-www-form-urlencoded; charset=UTF-8'
-  //       );
-  //       request.onreadystatechange = function() {
-  //         if (this.readyState == 4 && this.status == 201) {
-  //           data = JSON.parse(this.responseText);
-  //           renderTweets([data]);
-  //         }
-  //       };
-  //       request.send(`text=${tweetText}`);
-  //       elementsMap.textarea.value = '';
-  //       elementsMap.counter.textContent = elementsMap.validCounterLength;
-  //     }
-  //   }
-  // };
-
   //callback function for submitting a tweet
   const submitTweet = e => {
     event.preventDefault();
@@ -116,19 +70,6 @@ $(document).ready(() => {
     if (!hasError(tweetText)) {
       errorMessage = '';
       if (event.keyCode === 13 || e.type === 'submit') {
-        // const request = new XMLHttpRequest();
-        // request.open('POST', '/tweets', true);
-        // request.setRequestHeader(
-        //   'Content-Type',
-        //   'application/x-www-form-urlencoded; charset=UTF-8'
-        // );
-        // request.onreadystatechange = function() {
-        //   if (this.readyState == 4 && this.status == 201) {
-        //     data = JSON.parse(this.responseText);
-        //     renderTweets([data]);
-        //   }
-        // };
-
         $.ajax({
           type: 'POST',
           url: '/tweets',
@@ -147,18 +88,12 @@ $(document).ready(() => {
   };
 
   // added event listeners for submit and enter on form
-  console.log(elementsMap.tweetForm);
   $('.new-tweet form').on('keyup', submitTweet);
   $('.new-tweet form').on('submit', submitTweet);
 
   // add event listner to compose box
-  //elementsMap.compose.addEventListener('click', e => {
   $('.compose__box').on('click', e => {
-    //elementsMap.newTweet.classList.toggle('visible');
     $('.container .new-tweet').toggleClass('visible');
-    // elementsMap.newTweet.classList.toogle('animate1');
-    // elementsMap.newTweet.classList.toogle('animate2');
-    // elementsMap.textarea.focus();
     elementsMap.textarea.focus();
   });
 
@@ -169,27 +104,21 @@ $(document).ready(() => {
     let tweetAge = now - created_at;
     let sufix = '';
     // add the time stamp with correct sufix;
-    switch (true) {
-      case tweetAge >= 31557600000:
-        sufix += 'year ago';
-        tweetAge = Math.floor(tweetAge / 31557600000);
-        break;
-      case tweetAge >= 86400000:
-        sufix += 'days ago';
-        tweetAge = Math.floor(tweetAge / 86400000);
-        break;
-      case tweetAge >= 3600000:
-        sufix += 'hours ago';
-        tweetAge = Math.floor(tweetAge / 3600000);
-        break;
-      case tweetAge >= 60000:
-        sufix += 'minutes ago';
-        tweetAge = Math.floor(tweetAge / 60000);
-        break;
-      default:
-        sufix += 'seconds ago';
-        tweetAge = Math.floor(tweetAge / 1000);
-        break;
+    if (tweetAge >= 31557600000) {
+      sufix += 'year ago';
+      tweetAge = Math.floor(tweetAge / 31557600000);
+    } else if (tweetAge >= 86400000) {
+      sufix += 'days ago';
+      tweetAge = Math.floor(tweetAge / 86400000);
+    } else if (tweetAge >= 3600000) {
+      sufix += 'hours ago';
+      tweetAge = Math.floor(tweetAge / 3600000);
+    } else if (tweetAge >= 60000) {
+      sufix += 'minutes ago';
+      tweetAge = Math.floor(tweetAge / 60000);
+    } else {
+      sufix += 'Just now';
+      tweetAge = '';
     }
 
     return `<article class="tweet">
